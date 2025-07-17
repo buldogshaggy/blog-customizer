8b2356cbb330d4ce4dd04e2d876fe46ed5fcc735
buldogshaggy <buldo9shaggy@yandex.ru>
2025-07-17 20:04:43 +0700

    Доделал форму, но нет отступов в стилях формы

--- src/components/article-params-form/ArticleParamsForm.tsx
+++ src/components/article-params-form/ArticleParamsForm.tsx
@@ -2,11 +2,18 @@ import { ArrowButton } from 'src/ui/arrow-button';
 import { Button } from 'src/ui/button';
 import { useState } from 'react';
 import { Select } from 'src/ui/select';
+import { RadioGroup } from 'src/ui/radio-group';
+import { Separator } from 'src/ui/separator';
+import { Text } from 'src/ui/text';
 import {
 	fontFamilyOptions,
 	type OptionType,
 	defaultArticleState,
 	type ArticleStateType,
+	fontSizeOptions,
+	fontColors,
+	backgroundColors,
+	contentWidthArr,
 } from 'src/constants/articleProps';
 import clsx from 'clsx';
 
@@ -34,12 +41,40 @@ export const ArticleParamsForm = () => {
 					[styles.container_open]: isOpen,
 				})}>
 				<form className={styles.form}>
-					<h2 className={styles.title}>Задайте параметры</h2>
+					<Text as='h2' size={38} weight={800} uppercase>
+						Задайте параметры
+					</Text>
 					<Select
 						title='Шрифт'
 						selected={formData.fontFamilyOption}
 						onChange={handleChange('fontFamilyOption')}
 						options={fontFamilyOptions}
+					/>
+					<RadioGroup
+						selected={formData.fontSizeOption}
+						name='fontSize'
+						onChange={handleChange('fontSizeOption')}
+						options={fontSizeOptions}
+						title='Размер шрифта'
+					/>
+					<Select
+						title='Цвет шрифта'
+						selected={formData.fontColor}
+						onChange={handleChange('fontColor')}
+						options={fontColors}
+					/>
+					<Separator />
+					<Select
+						title='Цвет фона'
+						selected={formData.backgroundColor}
+						onChange={handleChange('backgroundColor')}
+						options={backgroundColors}
+					/>
+					<Select
+						title='Ширина контента'
+						selected={formData.contentWidth}
+						onChange={handleChange('contentWidth')}
+						options={contentWidthArr}
 					/>
 					<div className={styles.bottomContainer}>
 						<Button title='Сбросить' htmlType='reset' type='clear' />
